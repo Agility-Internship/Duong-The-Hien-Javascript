7a8d76eddb3f985c9e11a3317dd6758d6a9f21ea
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-05-15 14:24:48 +0700

    #20 Fix : Update format comment function

--- practice-01/js/index.js
+++ practice-01/js/index.js
@@ -3,20 +3,19 @@ const userName = document.querySelector('#fullname');
 const isEmail = document.querySelector('#email');
 const isAge = document.querySelector('#age');
 
-/* ------------------------------------*\
-  Show a message to the user when submitting
-\*------------------------------------*/
-
-// If false error message and invalid CSS
+/**
+ * Show a message to the user when submitting
+ */
 function showError(input, message) {
+    // If false error message and invalid CSS
     let errorDisplay = input.parentElement;
     let errorMessage = errorDisplay.querySelector('.form-message');
 
     errorDisplay.classList.add('invalid');
     errorMessage.innerText = message;
 }
-// If it's true, there's no message
 function showSuccess(input) {
+    // If it's true, there's no message
     let errorDisplay = input.parentElement;
     let errorMessage = errorDisplay.querySelector('.form-message');
 
@@ -24,13 +23,12 @@ function showSuccess(input) {
     errorMessage.innerText = '';
 }
 
-/* ------------------------------------*\
-  Validation check
-\*------------------------------------*/
-// Check the form, the user has entered it or not
-// Check if the user has entered or not,
-// Not > report an error
+/**
+ * Validation for the blank
+ */
 function checkEmtyError(listInput) {
+    // Check if the user has entered or not
+    // Not > report an error
     let isEmtyError = false;
     listInput.forEach(input => {
         input.value = input.value.trim();
@@ -44,7 +42,9 @@ function checkEmtyError(listInput) {
     });
     return isEmtyError;
 }
-// Validation for Email
+/**
+ * Validation for Email
+ */
 function checkEmailError(input) {
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     input.value = input.value.trim();
@@ -58,7 +58,9 @@ function checkEmailError(input) {
     }
     return isEmailError;
 }
-// Check the name has characters
+/**
+ * Validation for charcaters
+ */
 function checkCharacter(input) {
     const regexCharacter = /[ ^a-z-A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+$/;
 
@@ -68,14 +70,16 @@ function checkCharacter(input) {
         showError(input, 'Wrong input');
     }
 }
-// Check tra limit of number
+/**
+ * Validation for limit of number
+ */
 function checkLimitNumberError(input, min, max) {
 
     if (input.value < min) {
         showError(input, `Number must be larger than ${min}`);
         return true;
     }
-    if (input.value > max ) {
+    if (input.value > max) {
         showError(input, `Number must be small than ${max}`);
         return true;
     }
@@ -83,7 +87,9 @@ function checkLimitNumberError(input, min, max) {
     showSuccess(input);
     return false;
 }
-// event submit
+/**
+ * Event submit
+ */
 formElement.addEventListener('submit', function (e) {
     e.preventDefault()
 
