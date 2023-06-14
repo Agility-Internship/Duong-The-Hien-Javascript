8d2ac9f2631c888f9d5ea57359bcca98b1633853
Dương Thế Hiển <duongthehien2001@gmail.com>
2023-06-14 16:15:13 +0700

    Fix: Remove consolelog and edit style

--- practice-02/js/index.js
+++ practice-02/js/index.js
@@ -9,9 +9,7 @@ import LIST_PRODUCTS from '../database/products.json' assert {type: 'json'};
 function filterProductsByName(products, selectedBrands) {
     const filteredResults = [];
 
-    console.log(selectedBrands)
     selectedBrands.forEach(brand => {
-        console.log(brand)
         const filteredProducts = products.filter(product => product.name.toLowerCase().includes(brand.toLowerCase()));
         filteredResults.unshift(...filteredProducts);
     });
@@ -122,14 +120,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // Show/hide filter options on mouse hover
     filters.forEach(function (filter) {
         const filterShow = filter.querySelector('.filter-show');
+        const filterTitle = filter.querySelector('.filter-item__title')
 
         filter.addEventListener('mouseenter', function () {
-            filterShow.style.display = 'block';
-        });
+          filterShow.style.display = 'block';
+          filterTitle.style.border = '1px solid var(--secondary)';
+      });
 
-        filter.addEventListener('mouseleave', function () {
-            filterShow.style.display = 'none';
-        });
+      filter.addEventListener('mouseleave', function () {
+          filterShow.style.display = 'none';
+          filterTitle.style.border = '';
+      });
     });
 
     // Handle click event on logo buttons
@@ -144,7 +145,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (!sameLogoButton.parentNode.classList.contains('selected')) {
                     // Add 'selected' class and add product to selectedBrands
                     sameLogoButton.parentNode.classList.add('selected');
-                    console.log(sameLogoButton.parentNode)
                     if (isFirstIteration) {
                         selectedBrands.push(nameProduct);
                         isFirstIteration = false;
